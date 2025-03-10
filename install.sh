c3793180ee299f71a53c7e2bd3045e057541ed75
rizky supriatna <109171533+rizkylol@users.noreply.github.com>
2025-03-10 20:05:55 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'e502fe78a724b36e429b5a0ffe3f37af',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+</script>
+<script type="text/javascript" src="//asthmanotchcave.com/e502fe78a724b36e429b5a0ffe3f37af/invoke.js"></script>
