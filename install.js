3c32a860ab64a3d56894e258fefff67d2899289b
rizky supriatna <109171533+rizkylol@users.noreply.github.com>
2025-03-10 20:11:04 +0700

    Update install.js

--- install.js
+++ install.js
@@ -1,10 +1,11 @@
-<script type="text/javascript">
-	atOptions = {
-		'key' : 'e502fe78a724b36e429b5a0ffe3f37af',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
-	};
-</script>
-<script type="text/javascript" src="//asthmanotchcave.com/e502fe78a724b36e429b5a0ffe3f37af/invoke.js"></script>
+atOptions = {
+    'key': 'e502fe78a724b36e429b5a0ffe3f37af',
+    'format': 'iframe',
+    'height': 250,
+    'width': 300,
+    'params': {}
+};
+
+var script = document.createElement("script");
+script.src = "//asthmanotchcave.com/e502fe78a724b36e429b5a0ffe3f37af/invoke.js";
+document.body.appendChild(script);
