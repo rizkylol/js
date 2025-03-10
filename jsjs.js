1fed72c9936efd0c161540090296c577fbfc8ba7
rizky supriatna <109171533+rizkylol@users.noreply.github.com>
2025-03-10 20:40:38 +0700

    Create jsjs.js

---
+++ jsjs.js
@@ -0,0 +1,21 @@
+atOptions = {
+    'key': 'e502fe78a724b36e429b5a0ffe3f37af',
+    'format': 'iframe',
+    'height': 250,
+    'width': 300,
+    'params': {}
+};
+
+// Buat elemen <script> untuk iklan
+var script = document.createElement("script");
+script.src = "//asthmanotchcave.com/e502fe78a724b36e429b5a0ffe3f37af/invoke.js";
+
+// Coba cari elemen "#article-content" untuk meletakkan iklan di atas artikel
+var targetDiv = document.getElementById("article-content");
+
+if (targetDiv) {
+    targetDiv.insertBefore(script, targetDiv.firstChild);
+} else {
+    // Jika tidak ditemukan, letakkan di dalam <body> sebagai fallback
+    document.body.appendChild(script);
+}
