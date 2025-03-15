6f618ec5edf2f61039b73ab802e687556d36a1e5
rizky supriatna <109171533+rizkylol@users.noreply.github.com>
2025-03-15 16:38:19 +0700

    Create jsnew.js

---
+++ jsnew.js
@@ -0,0 +1,19 @@
+atOptions = {
+    'key': 'ecbe3100fe6772a0b7931be91ad8129d',
+    'format': 'iframe',
+    'height': 250,
+    'width': 300,
+    'params': {}
+};
+
+// myrtlebeachsafarifacts.com
+var script = document.createElement("script");
+script.src = "//asthmanotchcave.com/ecbe3100fe6772a0b7931be91ad8129d/invoke.js";
+
+var targetDiv = document.getElementById("article-content");
+
+if (targetDiv) {
+    targetDiv.insertBefore(script, targetDiv.firstChild);
+} else {
+    document.body.appendChild(script);
+}
