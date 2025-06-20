8dc8a1732985408115b41ab82ab9336c000abbc4
rizky supriatna <109171533+rizkylol@users.noreply.github.com>
2025-06-21 02:15:20 +0700

    Update jsnew.js

--- jsnew.js
+++ jsnew.js
@@ -7,5 +7,5 @@ atOptions = {
 };
 
 var script = document.createElement("script");
-script.src = "//asthmanotchcave.com/ecbe3100fe6772a0b7931be91ad8129d/invoke.js";
+script.src = "//bananascasketuntrue.com/ecbe3100fe6772a0b7931be91ad8129d/invoke.js";
 document.body.appendChild(script);
